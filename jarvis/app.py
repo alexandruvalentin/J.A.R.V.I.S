02c3d1dfe057e7e85a2c2fdb87423cd1a8fbbc2f
Alexandru Grapa <alexandru22valentin@gmail.com>
2021-12-11 08:34:01 +0000

    Add speak and greet function

--- jarvis/app.py
+++ jarvis/app.py
@@ -1,6 +1,9 @@
 import pyttsx3
 from decouple import config
 
+from datetime import datetime
+
+
 USERNAME = config('USER')
 BOTNAME = config('BOTNAME')
 
@@ -16,3 +19,24 @@ engine.setProperty('volume', 1.0)
 # Set Voice (Female)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
+
+
+# Text to Speech Conversion
+def speak(text):
+    """Used to speak whatever text is passed to it"""
+    
+    engine.say(text)
+    engine.runAndWait()
+    
+
+def greet_user():
+    """Greets the user according to the time"""
+    
+    hour = datetime.now().hour
+    if (hour >= 6) and (hour < 12):
+        speak(f"Good Morning {USERNAME}")
+    elif (hour >= 12) and (hour < 16):
+        speak(f"Good afternoon {USERNAME}")
+    elif (hour >= 16) and (hour < 19):
+        speak(f"Good Evening {USERNAME}")
+    speak(f"I am {BOTNAME}. How may I assist you?")
