86628c4b40d4107333ecbbb8bb5518c2dd788a7c
Alexandru Grapa <alexandru22valentin@gmail.com>
2021-12-13 15:26:48 +0000

    Add function to take user input

--- jarvis/app.py
+++ jarvis/app.py
@@ -3,6 +3,10 @@ from decouple import config
 
 from datetime import datetime
 
+import speech_recognition as sr
+from random import choice
+from utils import opening_text
+
 
 USERNAME = config('USER')
 BOTNAME = config('BOTNAME')
@@ -24,14 +28,14 @@ engine.setProperty('voice', voices[1].id)
 # Text to Speech Conversion
 def speak(text):
     """Used to speak whatever text is passed to it"""
-    
+
     engine.say(text)
     engine.runAndWait()
-    
 
+# Function to Greet User
 def greet_user():
     """Greets the user according to the time"""
-    
+
     hour = datetime.now().hour
     if (hour >= 6) and (hour < 12):
         speak(f"Good Morning {USERNAME}")
@@ -40,3 +44,31 @@ def greet_user():
     elif (hour >= 16) and (hour < 19):
         speak(f"Good Evening {USERNAME}")
     speak(f"I am {BOTNAME}. How may I assist you?")
+
+
+# Function to Take User Input
+def take_user_input():
+    """Takes user input, recognizes it using Speech Recognition module and converts it into text"""
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print('Recognizing...')
+        query = r.recognize_google(audio, language='en-in')
+        if not 'exit' in query or 'stop' in query:
+            speak(choice(opening_text))
+        else:
+            hour = datetime.now().hour
+            if hour >= 21 and hour < 6:
+                speak("Good night sir, take care!")
+            else:
+                speak('Have a good day sir!')
+            exit()
+    except Exception:
+        speak('Sorry, I could not understand. Could you please say that again?')
+        query = 'None'
+    return query
